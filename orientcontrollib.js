0be850b53da666ffdcf516ad99a73b89aca2426d
Anton Lenev <m0rg0t.Anton@gmail.com>
2017-12-15 20:33:13 +0300

    fixes

--- orientcontrollib.js
+++ orientcontrollib.js
@@ -1,9 +1,18 @@
 //[{maxAngle: 30, minAngle: 10, callback: function() {}},{callback: someFunc}]
+
+/**
+ * [OrientControl description]
+ * @param {[type]} params [description]
+ */
 function OrientControl(params) {
 	
 	var orientParams = [];
 	var isLast = false;
 	
+	/**
+	 * [addCorrectParams description]
+	 * @param {[type]} params [description]
+	 */
 	var addCorrectParams = function(params)
 	{
 			params.forEach(function(elem)
@@ -16,37 +25,44 @@ function OrientControl(params) {
 			});		
 	};
 	
-	addCorrectParams(params);
-	
+	/**
+	 * [checkOrientation description]
+	 * @param  {[type]} gamma [description]
+	 * @return {[type]}       [description]
+	 */
 	var checkOrientation = function(gamma)
 	{
-		this.orientParams.forEach(function(item)
+		orientParams.forEach(function(item)
 		{
 			if	((item.maxAngle > gamma) && (gamma > item.minAngle))
-				{
-
+			{
+				console.log("item", item, gamma);
 					if (item.isLast === true) {
 						isLast = true;
 						alert("isLast");
 					}
 					if  ((item.callback != undefined) && (item.callback != null))
 						item.callback();
-				}
-				
+			}	
 			
 		})
 	};
 	
+	/**
+	 * [receiveMessage description]
+	 * @return {[type]} [description]
+	 */
 	var receiveMessage = function()
 	{
 		if (isLast !== true) {
-		 if ((event.data!==null) && (event.data!==undefined)) {
-                    if (event.data.message === "probtn_page_deviceorientation") {
-                        checkOrientation(event.data.dataEvent.gamma);
-                    }
-                }
+			if ((event.data!==null) && (event.data!==undefined)) {
+            	if (event.data.message === "probtn_page_deviceorientation") {
+                	checkOrientation(event.data.dataEvent.gamma);
+            	}
+        	}
 		}
 	};
 	
+	addCorrectParams(params);
 	window.addEventListener("message", receiveMessage, false);
 }
